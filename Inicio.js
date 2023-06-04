e78906758445b5e1953f104c179137246db01fee
FacuBasile <128640163+FacuBasile@users.noreply.github.com>
2023-06-04 02:18:08 -0300

    falta form y carrusel

---
+++ Inicio.js
@@ -0,0 +1,35 @@
+const carrusel = document.getElementsByClassName("fotosCarrusel");
+
+
+
+
+let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo = null;
+let step = 1;
+const start = () => {
+    intervalo = setInterval(function () {
+        carrusel.scrollLeft = carrusel.scrollLeft + step;
+        if (carrusel.scrollLeft >= maxScrollLeft - 1) {
+            step = -1;
+        } else if (carrusel.scrollLeft <= 1) {
+            step = 1;
+        }
+    }, 10);
+};
+
+const stop = () => {
+    clearInterval(intervalo);
+};
+
+carrusel.addEventListener("mouseover", () => {
+    stop();
+});
+
+carrusel.addEventListener("mouseout", () => {
+    start();
+});
+
+start();
+
+const BTNbuscarProv = document.getElementById("buscarProv");
+
